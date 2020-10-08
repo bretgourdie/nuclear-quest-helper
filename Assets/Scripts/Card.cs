40729229dc3ec1f00cd1f4292dc7c9897ffa4e31
Bret Gourdie <BretGourdie@gmail.com>
2020-10-08 00:40:05 -0400

    Add Assets

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    private bool isBeingHeld;
+
+    private float startPositionX;
+    private float startPositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleDrag();
+    }
+
+    private void handleDrag()
+    {
+        if (isBeingHeld)
+        {
+            var mousePosition = getMousePosition();
+
+            gameObject.transform.localPosition = new Vector3(
+                mousePosition.x - startPositionX,
+                mousePosition.y - startPositionY,
+                this.transform.localPosition.z);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isBeingHeld = true;
+            var mousePosition = getMousePosition();
+
+            startPositionX = mousePosition.x - this.transform.localPosition.x;
+            startPositionY = mousePosition.y - this.transform.localPosition.y;
+        }
+    }
+
+    private Vector3 getMousePosition()
+    {
+        var mousePosition = Input.mousePosition;
+        var mousePositionWorldPoint = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        return mousePositionWorldPoint;
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+    }
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    [SerializeField] List<Card> Cards;
+
+    private Queue<Card> cardQueue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var cards = new List<Card>(Cards);
+        while (cards.Count > 0)
+        {
+            var randomIndex = Random.Range(0, cards.Count);
+
+            var removed = cards[randomIndex];
+
+            cards.RemoveAt(randomIndex);
+
+            cardQueue.Enqueue(removed);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Card DrawCard()
+    {
+        if (cardQueue.Count > 0)
+        {
+            return cardQueue.Dequeue();
+        }
+
+        return null;
+    }
+
+    public void DiscardCard(Card card)
+    {
+        cardQueue.Enqueue(card);
+    }
+}
