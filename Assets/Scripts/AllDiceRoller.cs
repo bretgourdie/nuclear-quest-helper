558b60608adca07549c26b295a5f748832450980
Bret Gourdie <BretGourdie@gmail.com>
2020-10-16 00:38:18 -0400

    Add dice roller

---
+++ Assets/Scripts/AllDiceRoller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllDiceRoller : MonoBehaviour
+{
+    [SerializeField] List<GameObject> DiceToRoll = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RollAllDice()
+    {
+        foreach (var dice in DiceToRoll)
+        {
+            dice.GetComponent<Dice>().Roll();
+        }
+    }
+}
---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dice : MonoBehaviour
+{
+    private Text _buttonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var button = gameObject.GetComponent<Button>();
+        if (button != null)
+        {
+            _buttonText = button.GetComponentInChildren<Text>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Roll()
+    {
+        StartCoroutine(PerformRoll());
+    }
+
+    IEnumerator PerformRoll()
+    {
+        _buttonText.text = "?";
+        yield return new WaitForSeconds(0.25f);
+        var rolled = Random.Range(1, 6 + 1);
+        _buttonText.text = rolled.ToString();
+    }
+}
