a52a403c58524ce08b5abd4eb9518bad384cda69
Bret Gourdie <BretGourdie@gmail.com>
2020-10-20 22:13:45 -0400

    Add Turn Order tracker

---
+++ Assets/Scripts/TurnOrderButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOrderButton : MonoBehaviour
+{
+    [SerializeField] int PlayerNumber = default;
+    [SerializeField] GameObject TurnOrderPrefab = default;
+
+    private TurnOrderContainer _turnOrderContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _turnOrderContainer = TurnOrderPrefab.GetComponent<TurnOrderContainer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeToPlayerTurn()
+    {
+        _turnOrderContainer.GoToTurn(PlayerNumber);
+    }
+}
---
+++ Assets/Scripts/TurnOrderContainer.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnOrderContainer : MonoBehaviour
+{
+    [SerializeField] List<GameObject> TurnButtonPrefabs = default;
+    [SerializeField] GameObject NextTurnPrefab = default;
+    [SerializeField] Color CurrentTurnColor = Color.red;
+
+    private int currentTurnIndex = 0;
+    private ColorBlock _currentTurnColors;
+    private ColorBlock _notTurnColors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (TurnButtonPrefabs.Count > 0)
+        {
+            var button = TurnButtonPrefabs[0].GetComponent<Button>();
+
+            if (button != null)
+            {
+                initializeColorBlocks(button.colors);
+                button.colors = _currentTurnColors;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void initializeColorBlocks(ColorBlock template)
+    {
+        _notTurnColors = template;
+
+        _currentTurnColors = template;
+        _currentTurnColors.normalColor = CurrentTurnColor;
+        _currentTurnColors.highlightedColor = CurrentTurnColor;
+        _currentTurnColors.selectedColor = CurrentTurnColor;
+    }
+
+    public void AdvanceTurn()
+    {
+        handleTurnChange((currentTurnIndex + 1) % TurnButtonPrefabs.Count);
+    }
+
+    private void handleTurnChange(int nextPlayerIndex)
+    {
+        var endingTurnButton = TurnButtonPrefabs[currentTurnIndex];
+
+        setButtonColor(endingTurnButton, _notTurnColors);
+
+        currentTurnIndex = nextPlayerIndex;
+
+        var newTurnButton = TurnButtonPrefabs[currentTurnIndex];
+
+        setButtonColor(newTurnButton, _currentTurnColors);
+    }
+
+    public void GoToTurn(int player)
+    {
+        handleTurnChange(player - 1);
+    }
+
+    private void setButtonColor(GameObject buttonGameObject, ColorBlock colors)
+    {
+        var button = buttonGameObject.GetComponent<Button>();
+
+        button.colors = colors;
+    }
+}
